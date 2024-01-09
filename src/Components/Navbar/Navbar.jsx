b11d3b3470004b006d348c3f201d7fc8d80faf37
Rahul <royalchauhan300@gmail.com>
2024-01-09 15:13:26 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+        <li>
+          <Link to="/mens">Men</Link>
+        </li>
+        <li>
+          <Link to="/womens">Women</Link>
+        </li>
+        <li>
+          <Link to="/kids">Kids</Link>
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
