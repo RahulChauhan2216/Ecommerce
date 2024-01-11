9c953ef7eba84ebb310488eaf342e1cf1475d254
Rahul <royalchauhan300@gmail.com>
2024-01-11 21:31:31 +0530

    till add to cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Shopcategory from "./Pages/Shopcategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import Loginsignup from "./Pages/Loginsignup";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
@@ -14,9 +17,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<Shopcategory category="men" />} />
-          <Route path="/womens" element={<Shopcategory category="women" />} />
-          <Route path="/kids" element={<Shopcategory category="kid" />} />
+          <Route path="/mens" element={<Shopcategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<Shopcategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<Shopcategory banner={kid_banner} category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrums from "../Components/Breadcrums/Breadcrums";
+import Productdisplay from "../Components/Productdisplay/Productdisplay";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
 
-export default Product
+  return (
+    <div>
+      <Breadcrums product={product} />
+      <Productdisplay product={product} />
+    </div>
+  );
+};
+
+export default Product;
